285a749f308917ac2d28649ba2ef4c5ab06aa5ee
Nikolay Nozdrin.Plotnitsky@gmail.com <nozdrin.plotnitsky@gmail.com>
2013-05-17 20:19:36 +0300

    Доработал внешний вид...

--- io.h
+++ io.h
@@ -1,7 +1,20 @@
 #ifndef __IO_H__
 #define __IO_H__
 
-void generalMenu(void);
-//void clearScreen(void);
+#define X_MAX	80
+#define Y_MAX 25
+
+enum {
+	GENERAL,
+	BALANS,
+	PROFIT,
+	EXPANSE,
+	ACTIVE,
+	PASSIVE,
+	EXIT
+};
+
+void clearScreen(void);
+void gotoxy(int x, int y);
 
 #endif
--- main.c
+++ main.c
@@ -3,12 +3,100 @@
 
 void generalMenu(void);
 
+int active = 1;
+int passive = 2;
+int profit = 3;
+int expanse = 4;
+int balanse = -2;
+
+void generalMenu(void);
+void menuBalans(void);
+void menuProfit(void);
+void menuExpanse(void);
+void menuActive(void);
+void menuPassive(void);
+FILE* openBalans(char* path);
+
+unsigned int go_menu = GENERAL;
 int main(void)
 	{
-		while(1)
+		while(go_menu != EXIT )
 			{
-				generalMenu();
+				switch(go_menu)
+					{
+					case GENERAL:	generalMenu();	break;
+					case BALANS:	menuBalans();		break;
+					case PROFIT:	menuProfit();		break;
+					case EXPANSE:	menuExpanse();	break;
+					case ACTIVE:	menuActive();		break;
+					case PASSIVE: menuPassive();	break;
+					default:break;
+					}
 			}
 		return 0;
 	}
 
+void 
+ generalMenu(void)
+	{
+		//unsigned int x, y;
+		//clearScreen();
+		//x = X_MAX/2; y = Y_MAX/2;
+		gotoxy(x,y);
+		printf("//--------------------------------//\n");
+		printf("Сделайте выбор:\n1.Расчет баланса\n2.Доходы\n3.Расходы\n4.Активы\n5.Пассивы\n");
+		printf("6.Задать файл истории\n");
+		scanf("%d",&go_menu);
+		getchar();
+		if(go_menu == 0)	go_menu= EXIT;
+	}
+
+void
+	menuBalans(void)
+	{
+		FILE* file;
+		balanse = active - passive;
+	  file = fopen("./history/012013.hry","w");
+		fputc('k', file);
+		fclose(file);
+		file = openBalans("./history/012013.hry");
+		printf("Активы: %d\nПассивы: %d\nБаланс: %d\n", profit, expanse, balanse);
+	}
+
+void 
+	menuProfit(void)
+	{
+		printf("%d\n", profit);
+		scanf("%d", &profit);
+		getchar();
+	}
+
+void
+	menuExpanse(void)
+	{
+		printf("%d\n",expanse);
+		scanf("%d", &expanse);
+		getchar();
+	}
+
+void 
+	menuActive(void)
+	{
+		printf("%d\n", active);
+		scanf("%d", &active);
+		getchar();
+	}
+
+void menuPassive(void)
+	{
+		printf("%d\n", passive);
+		scanf("%d", &passive);
+		getchar();
+	}
+
+FILE* openBalans(char* path)
+	{
+		FILE* f;
+		f = fopen(path,"r");
+		return f;
+	}
